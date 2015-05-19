4574d5d4a4fdffd12e4161b8987f050e16f87fad
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2015-05-19 16:45:06 +0100

    Append to window.onload, don't override

--- js/no-history.js
+++ js/no-history.js
@@ -10,24 +10,27 @@
 
 // Prevent errors in in-supporting browsers (old IE and old FF):
 if (document.getElementsByClassName) {
-
-    // Plain JS version:
-    var links = document.getElementsByClassName('js-no-history');
-    var l     = links.length;
-    for(var i = 0; i < l; i++)
-    {
-        links.item(i).addEventListener('click', function(e) {
-            e.preventDefault();
-            location.replace(this.href);
-        }, false);
-    }
-    
-        // jQuery version:
-    /*
-    $(".js-no-history").on("click", function(e) {
-        e.preventDefault();
-        location.replace(e.target.href)
-    });
-    */
-    
+    window.onload = (function(onload) {
+        return function(event) {
+            
+            // Plain JS version:
+            var links = document.getElementsByClassName('js-no-history');
+            var l     = links.length;
+            for(var i = 0; i < l; i++)
+            {
+                links.item(i).addEventListener('click', function(e) {
+                    e.preventDefault();
+                    location.replace(this.href);
+                }, false);
+            }
+            
+            // jQuery version:
+            /*
+            $(".js-no-history").on("click", function(e) {
+                e.preventDefault();
+                location.replace(e.target.href)
+            });
+            */
+        }
+    }(window.onload));
 }
